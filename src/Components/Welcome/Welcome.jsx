de70ee398b43c9e3cef2e4887e0d05a4541ae128
Ginura Premawardana <126656500+GinuraJ@users.noreply.github.com>
2025-09-06 01:38:00 +0530

    welcome note changing

--- src/Components/Welcome/Welcome.jsx
+++ src/Components/Welcome/Welcome.jsx
@@ -4,6 +4,8 @@ export default function Welcome(){
     return(
         <div className="home h-full w-full">
             <h1>Welcome</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus blanditiis unde ullam? Omnis, voluptatum sunt! Veniam, perspiciatis aliquam possimus obcaecati quidem earum. Expedita, non temporibus at quibusdam excepturi itaque nulla!</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus blanditiis unde ullam? Omnis, voluptatum sunt! Veniam, perspiciatis aliquam possimus obcaecati quidem earum. Expedita, non temporibus at quibusdam excepturi itaque nulla!</p>
         </div>
     )
 }
